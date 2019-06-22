18d77a91a6d2f1d8f7396a90bcf16bc7256bb382
woyu <1531085017@qq.com>
2019-06-22 21:38:34 +0800

    用了seleniumm

--- fanren_spider.py
+++ fanren_spider.py
@@ -1,30 +1,56 @@
-import requests
-from bs4 import BeautifulSoup
-from lxml import etree
 
-page=1
+from selenium import webdriver
 
-url='https://www.ysts8.com/play_1836_49_1_'+str(page)+'.html'
-
-def get_html(url):
-    response=requests.get(url)
-    response.encoding='gb2312'
-    html_tree=etree.HTML(response.text,etree.HTMLParser())
-    iframe_src=html_tree.xpath('//*[@id="play"]/@src')
-    root_url='https://www.ysts8.com'
-    result_url=root_url+iframe_src[0]
-    print(result_url)
-    response=requests.get(result_url)
-    response.encoding='gb2312'
-    html_tree=etree.HTML(response.text,etree.HTMLParser())
-    print(response.text)
-    #发现页面是通过js脚本渲染出来的，所以下面执行不成功
-    audio_url=html_tree.xpath('//*[@id="jp_audio_0"]/@src')
-    print(audio_url)
-    return audio_url
 def get_content(url,name):
-    content=requests.get(url).content
+    try:
+        content=requests.get(url).content
+    except:
+        print('音频资源失效')
+        return  
     with open(name+'.mp3','wb') as m:
         m.write(content)
         m.close()
-get_html(url)
+def main():
+    for page in range(1,1632):
+        url='https://www.ysts8.com/play_1836_49_1_'+str(page)+'.html'
+        browser=webdriver.Chrome()
+        try:
+            browser.get(url)
+        except:
+            with open('fail.txt','a+') as n:
+                n.write(url+'\n')
+                n.close()
+            print('失效网址：'+url)
+            browser.close()
+            return
+        try:
+            browser.switch_to.frame('play')
+        except:
+            print('没有id或class为play的ifrmae标签')
+            with open('fail.txt','a+') as n:
+                n.write(url+'\n')
+                n.close()
+            browser.close()
+            return
+        try:
+            audio=browser.find_element_by_xpath('//*[@id="jp_audio_0"]')
+        except:
+            print('未找到audio标签')
+            with open('fail.txt','a+') as n:
+                n.write(url+'\n')
+                n.close()
+            browser.close()
+            return
+        try:    
+            get_content(audio.get_attribute('src'),str(page))
+        except:
+            with open('fail.txt','a+') as n:
+                n.write(url+'\n')
+                n.close()
+            browser.close()
+            return
+if __name__=='__main__':
+    main()
+
+
+
